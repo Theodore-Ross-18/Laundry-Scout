2a3f7a340ec2f0addb849fb60c55d6c55440be61
Theodore-Ross-18 <theodoreross18@gmail.com>
2025-08-21 16:09:03 +0800

    TEST4

--- laundry_scout/build.sh
+++ laundry_scout/build.sh
@@ -17,7 +17,9 @@ flutter config --enable-web
 flutter pub get
 
 # Build web app with environment variables passed from Vercel
-flutter build web --release --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
+flutter build web --release \
+  --dart-define=SUPABASE_URL=${SUPABASE_URL:-https://aoyaedzbgollhajvrxiu.supabase.co} \
+  --dart-define=SUPABASE_ANON_KEY=${SUPABASE_ANON_KEY:-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFveWFlZHpiZ29sbGhhanZyeGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyNzY1NzUsImV4cCI6MjA2Mjg1MjU3NX0.iShQfGX-jB7798jk6fLim6m_eGpupzPb8lVgEBTMd1U}
 
 # Copy .env file to build/web if it exists
 if [ -f ".env" ]; then
