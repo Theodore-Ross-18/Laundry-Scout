7b833ff004df84334ce594c5370d6f74d038be5d
Theodore-Ross-18 <theodoreross18@gmail.com>
2025-08-29 20:57:45 +0800

    Fixes

---
+++ laundry_scout/lib/helpers/timezone_helper.dart
@@ -0,0 +1,26 @@
+class TimezoneHelper {
+  static const List<String> commonTimezones = [
+    'UTC',
+    'America/New_York',
+    'America/Los_Angeles',
+    'America/Chicago',
+    'Europe/London',
+    'Europe/Paris',
+    'Asia/Tokyo',
+    'Asia/Shanghai',
+    'Australia/Sydney',
+  ];
+  
+  static String getCurrentTimezone() {
+    return DateTime.now().timeZoneName;
+  }
+  
+  static String getTimezoneOffset() {
+    final now = DateTime.now();
+    final offset = now.timeZoneOffset;
+    final hours = offset.inHours;
+    final minutes = (offset.inMinutes % 60).abs();
+    final sign = hours >= 0 ? '+' : '-';
+    return '${sign}${hours.abs().toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}';
+  }
+}
