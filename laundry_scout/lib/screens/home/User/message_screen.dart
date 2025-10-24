c34157dc4d39da2904fdcda653281b74f23c765a
Theodore-Ross-18 <theodoreross18@gmail.com>
2025-10-25 07:04:38 +0800

    Nice

--- laundry_scout/lib/screens/home/User/message_screen.dart
+++ laundry_scout/lib/screens/home/User/message_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_field
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
